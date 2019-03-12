185947ac8bd54ee6dc016b4a95c3969aa1509a6e
sawahata <suzumenodance@gmail.com>
2019-03-12 23:32:14 +0900

    クラスの追加

--- Iterator/Iterator.pas
+++ Iterator/Iterator.pas
@@ -8,34 +8,75 @@ type
             p_Name : String;
             p_Sex : Integer;
         public
-            constructor AfterConstruction(name : String; sex : Integer);
+            constructor Create(name : String; sex : Integer);
 
             function GetName : String;
             function GetSex : Integer;
-    end;
+        end;
+
+
+    TStudentList = class
+        private
+            p_students : array of TStudent;
+            p_last : Integer;
+        public
+            constructor Create(studentCount : Integer);
+
+            procedure Add(student : TStudent);
+            function GetStudentAt(index : Integer) : TStudent;
+            function GetLastNum : Integer;
+
+        end;
 
     TIterator = class
 
-    end;
+        end;
+
+
 
 implementation
 
 { TStudent }
 
-constructor TStudent.AfterConstruction(name : String; sex : Integer);
+constructor TStudent.Create(name : String; sex : Integer);
     begin
-    p_Name := name;
-    p_Sex := sex;
+    Self.p_Name := name;
+    Self.p_Sex := sex;
     end;
 
 function TStudent.GetName: String;
     begin
-    Result := p_Name;
+    Result := Self.p_Name;
     end;
 
 function TStudent.GetSex: Integer;
     begin
-    Result := p_Sex;
+    Result := Self.p_Sex;
     end;
+
+{ TStudentList }
+
+constructor TStudentList.Create(studentCount: Integer);
+    begin
+    SetLength(Self.p_students, studentCount);
+    end;
+
+procedure TStudentList.Add(student: TStudent);
+    begin
+    Self.p_students[Self.p_last] := student;
+    Inc(Self.p_last, 1);
+    end;
+
+function TStudentList.GetStudentAt(index: Integer): TStudent;
+    begin
+    Result := Self.p_students[index];
+    end;
+
+function TStudentList.GetLastNum: Integer;
+    begin
+    Result := Self.p_last;
+    end;
+
+
 
 end.
